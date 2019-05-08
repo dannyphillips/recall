8074a7989641837d0e02e47620cb5c1e74c2f5ea
Danny Phillips <danny.phillips8@gmail.com>
2019-05-08 15:30:56 -0700

    add hello slack

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const request = require("request");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  response.send("Hello from Firebase!");
+});
+
+exports.helloSlack = functions.https.onRequest(() => {
+  return request.post("url", {
+    json: {
+      text: `Hello Slack, this is Recall!`
+    }
+  });
 });
