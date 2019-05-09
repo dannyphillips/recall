993f680fd288b8732c77cf3806f8521cb37b1944
Danny Phillips <danny.phillips8@gmail.com>
2019-05-08 21:42:58 -0700

    use async/await

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,10 @@
 const functions = require("firebase-functions");
-const request = require("request");
+  const IncomingWebhook = require("@slack/webhook");
 
-const slackWebhook =
-  "https://hooks.slack.com/services/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";
+const SLACK_WEBHOOK_URL =
+  " https://hooks.slack.com/services/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";
+// const slack = require("slack-notify")(MY_SLACK_WEBHOOK_URL);
+
 
 // Prints Hello World in Browser
 exports.helloWorld = functions.https.onRequest((request, response) => {
@@ -10,56 +12,65 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 // Posts Hello World to Slack
+// exports.helloSlack = functions.https.onRequest((r, response) => {
+//   response.send("Successfully posted message to slack!");
+//   slack.send({
+//     channel: "#general",
+//     icon_url: "http://example.com/my-icon.png",
+//     text: `Hello Slack, this is Recall!`,
+//     unfurl_links: 1,
+//     username: "Jimmy"
+//   });
+// });
+
 exports.helloSlack = functions.https.onRequest((r, response) => {
   response.send("Successfully posted message to slack!");
-  debugger;
-  return request.post(slackWebhook, {
-    json: {
-      text: `Hello Slack, this is Recall!`
-    }
-  });
+  const webhook = new IncomingWebhook(SLACK_WEBHOOK_URL);
+
+  // Send the notification
+  (async () => {
+    await webhook.send({
+      text: "I've got news for you..."
+    });
+  })();
 });
 
 // Posts Daily TIL Form
-exports.recallDaily = functions.https.onRequest((r, response) => {
-  response.send("Successfully posted message to slack!");
-  debugger;
-  return request.post(slackWebhook, {
-    json: {
-      text: `Hello Slack, this is Recall!`
-    }
-  });
-});
+// exports.recallDaily = functions.https.onRequest((r, response) => {
+//   response.send("Successfully posted message to slack!");
+//   return request.post(slackWebhook, {
+//     json: {
+//       text: `Hello Slack, this is Recall!`
+//     }
+//   });
+// });
 
-// Posts Weekly Digest Poll
-exports.recallWeekly = functions.https.onRequest((r, response) => {
-  response.send("Successfully posted message to slack!");
-  debugger;
-  return request.post(slackWebhook, {
-    json: {
-      text: `Hello Slack, this is Recall!`
-    }
-  });
-});
+// // Posts Weekly Digest Poll
+// exports.recallWeekly = functions.https.onRequest((r, response) => {
+//   response.send("Successfully posted message to slack!");
+//   return request.post(slackWebhook, {
+//     json: {
+//       text: `Hello Slack, this is Recall!`
+//     }
+//   });
+// });
 
-// Posts Monthly Digest Poll
-exports.recallMonthly = functions.https.onRequest((r, response) => {
-  response.send("Successfully posted message to slack!");
-  debugger;
-  return request.post(slackWebhook, {
-    json: {
-      text: `Hello Slack, this is Recall!`
-    }
-  });
-});
+// // Posts Monthly Digest Poll
+// exports.recallMonthly = functions.https.onRequest((r, response) => {
+//   response.send("Successfully posted message to slack!");
+//   return request.post(slackWebhook, {
+//     json: {
+//       text: `Hello Slack, this is Recall!`
+//     }
+//   });
+// });
 
-// Posts Random Digest Poll
-exports.recallRandom = functions.https.onRequest((r, response) => {
-  response.send("Successfully posted message to slack!");
-  debugger;
-  return request.post(slackWebhook, {
-    json: {
-      text: `Hello Slack, this is Recall!`
-    }
-  });
-});
+// // Posts Random Digest Poll
+// exports.recallRandom = functions.https.onRequest((r, response) => {
+//   response.send("Successfully posted message to slack!");
+//   return request.post(slackWebhook, {
+//     json: {
+//       text: `Hello Slack, this is Recall!`
+//     }
+//   });
+// });
