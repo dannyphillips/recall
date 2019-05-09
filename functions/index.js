46ee355ad5d3eaba12219dcf35e5f9e7b44acb06
Danny Phillips <danny.phillips8@gmail.com>
2019-05-08 20:12:40 -0700

    updated slack webhook url

--- functions/index.js
+++ functions/index.js
@@ -8,10 +8,14 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
 
-exports.helloSlack = functions.https.onRequest(() => {
-  return request.post("url", {
-    json: {
-      text: `Hello Slack, this is Recall!`
+exports.helloSlack = functions.https.onRequest((r, response) => {
+  request.post(
+    "https://hooks.slack.com/services/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",
+    {
+      json: {
+        text: `Hello Slack, this is Recall!`
+      }
     }
-  });
+  );
+  response.send("Successfully posted message to slack!");
 });
